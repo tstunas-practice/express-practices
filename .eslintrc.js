cbf3c54275986ce49d6a38506cdebffd81ed1897
Park Jin <qkrwls6521@gmail.com>
2022-12-20 18:40:33 +0900

    add: eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: false,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {},
+};
