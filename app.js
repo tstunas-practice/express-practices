0db5ef45f71f77468c51534972945e7889e717c7
Park Jin <qkrwls6521@gmail.com>
2022-12-21 16:59:43 +0900

    add cookieparser

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const routes = require("./routes");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
