f9362fe0a496ecdb530190d2deda0931b2b153db
Park Jin <qkrwls6521@gmail.com>
2022-12-21 09:33:21 +0900

    delete: schema

--- schemas/index.js
+++
@@ -1,9 +0,0 @@
-const { postSchema, Post } = require("./post");
-const { commentSchema, Comment } = require("./comment");
-
-module.exports = {
-  postSchema,
-  commentSchema,
-  Post,
-  Comment
-};
